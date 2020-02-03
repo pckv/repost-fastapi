e93062fe40496c48be6878837ce9eb608c80120e
Per Christian Kvalvik <pc@kvalvik.no>
2020-02-03 13:49:27 +0100

    Document API and move documentation urls

--- repost/main.py
+++ repost/main.py
@@ -1,6 +1,10 @@
+""" Repost API written in FastAPI. """
+__version__ = '0.0.1'
+
 from fastapi import FastAPI
 
 from repost.api import api_router
 
-app = FastAPI()
+app = FastAPI(title='Repost', version=__version__, description=__doc__,
+              docs_url='/api/swagger', redoc_url='/api/docs')
 app.include_router(api_router, prefix='/api')
