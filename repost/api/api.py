ae99cb260bb15e9ff111008d845e829a1484bd9f
Per Christian Kvalvik <pc@kvalvik.no>
2020-02-03 10:24:41 +0100

    Update formatting

--- repost/api/api.py
+++ repost/api/api.py
@@ -3,4 +3,4 @@ from fastapi import APIRouter
 from repost.api.routes import users
 
 api_router = APIRouter()
-api_router.include_router(users.router, prefix='/users', tags=['Users'])
+api_router.include_router(users.router, prefix='/users', tags=['users'])
