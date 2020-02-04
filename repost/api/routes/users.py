573aa86541ead7e0ca2ddd9ef83cdab0bcec26bf
Per Christian Kvalvik <pc@kvalvik.no>
2020-02-04 11:51:00 +0100

    Add Error response model and document all possible responses

--- repost/api/routes/users.py
+++ repost/api/routes/users.py
@@ -1,59 +1,65 @@
 from typing import List
 
 from fastapi import APIRouter, Depends
-from starlette.status import HTTP_201_CREATED, HTTP_404_NOT_FOUND, HTTP_400_BAD_REQUEST
+from starlette.status import HTTP_201_CREATED, HTTP_404_NOT_FOUND, HTTP_400_BAD_REQUEST, HTTP_403_FORBIDDEN
 
-from repost.api.schemas import User, UserCreate, Resub, Post, Comment
+from repost.api.schemas import User, UserCreate, Resub, Post, Comment, ErrorResponse
 from repost.api.security import get_current_user
 
 router = APIRouter()
 
 
 @router.post('/', response_model=User, status_code=HTTP_201_CREATED,
-             responses={HTTP_400_BAD_REQUEST: {'description': 'Username taken'}})
+             responses={HTTP_400_BAD_REQUEST: {'description': 'Username taken', 'model': ErrorResponse}})
 async def create_user(user: UserCreate):
     """ """
     pass
 
 
-@router.get('/me', response_model=User)
+@router.get('/me', response_model=User,
+            responses={HTTP_403_FORBIDDEN: {'description': 'Invalid authorization', 'model': ErrorResponse}})
 async def get_current_user(current_user: User = Depends(get_current_user)):
     """ """
     pass
 
 
-@router.patch('/me', response_model=User)
+@router.patch('/me', response_model=User,
+              responses={HTTP_403_FORBIDDEN: {'description': 'Invalid authorization', 'model': ErrorResponse}})
 async def edit_current_user(current_user: User = Depends(get_current_user)):  # TODO: Finish endpoint
     """ """
     pass
 
 
-@router.delete('/me')
+@router.delete('/me',
+               responses={HTTP_403_FORBIDDEN: {'description': 'Invalid authorization', 'model': ErrorResponse}})
 async def delete_current_user(current_user: User = Depends(get_current_user)):
     """ """
     pass
 
 
 @router.get('/{username}', response_model=User,
-            responses={HTTP_404_NOT_FOUND: {'description': 'User not found'}})
+            responses={HTTP_404_NOT_FOUND: {'description': 'User not found', 'model': ErrorResponse}})
 async def get_user(username: str):
     """ """
     pass
 
 
-@router.get('/{username}/resubs', response_model=List[Resub])
+@router.get('/{username}/resubs', response_model=List[Resub],
+            responses={HTTP_404_NOT_FOUND: {'description': 'User not found', 'model': ErrorResponse}})
 async def get_resubs_owned_by_user(username: str):
     """ """
     pass
 
 
-@router.get('/{username}/posts', response_model=List[Post])
+@router.get('/{username}/posts', response_model=List[Post],
+            responses={HTTP_404_NOT_FOUND: {'description': 'User not found', 'model': ErrorResponse}})
 async def get_posts_by_user(username: str):
     """ """
     pass
 
 
-@router.get('/{username}/comments', response_model=List[Comment])
+@router.get('/{username}/comments', response_model=List[Comment],
+            responses={HTTP_404_NOT_FOUND: {'description': 'User not found', 'model': ErrorResponse}})
 async def get_comments_by_user(username: str):
     """ """
     pass
--- repost/api/schemas/__init__.py
+++ repost/api/schemas/__init__.py
@@ -3,3 +3,4 @@ from .resub import *
 from .post import *
 from .comment import *
 from .auth import *
+from .error import *
