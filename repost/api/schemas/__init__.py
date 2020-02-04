ebe445e74b9e26ae47c2a747bca6c0e3fc7d16d1
Per Christian Kvalvik <pc@kvalvik.no>
2020-02-04 11:53:52 +0100

    Change to absolute imports

--- repost/api/schemas/__init__.py
+++ repost/api/schemas/__init__.py
@@ -1,6 +1,6 @@
-from .user import *
-from .resub import *
-from .post import *
-from .comment import *
-from .auth import *
-from .error import *
+from .user import User, UserCreate
+from .resub import Resub
+from .post import Post
+from .comment import Comment
+from .auth import OAuth2Token
+from .error import ErrorResponse
