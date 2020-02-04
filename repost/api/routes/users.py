d70a850197d516c84fdb79d3f73e4e364c1bb6d5
Per Christian Kvalvik <pc@kvalvik.no>
2020-02-04 11:27:05 +0100

    Change order of user endpoints:
    /{str} endpoints will override any custom endpoints, such as the /me endpoint. Therefore, /me must be defined before /{str}

--- repost/api/routes/users.py
+++ repost/api/routes/users.py
@@ -8,34 +8,9 @@ from repost.api.schemas import User, UserCreate, Resub, Post, Comment
 router = APIRouter()
 
 
-@router.get('/{username}', response_model=User,
-            responses={HTTP_404_NOT_FOUND: {'description': 'User not found'}})
-async def get_user(username: str):
-    """ """
-    pass
-
-
 @router.post('/', response_model=User, status_code=HTTP_201_CREATED,
              responses={HTTP_400_BAD_REQUEST: {'description': 'Username taken'}})
 async def create_user(user: UserCreate):
-    """ """
-    pass
-
-
-@router.get('/{username}/resubs', response_model=List[Resub])
-async def get_resubs_owned_by_user(username: str):
-    """ """
-    pass
-
-
-@router.get('/{username}/posts', response_model=List[Post])
-async def get_posts_by_user(username: str):
-    """ """
-    pass
-
-
-@router.get('/{username}/comments', response_model=List[Comment])
-async def get_comments_by_user(username: str):
     """ """
     pass
 
@@ -54,5 +29,30 @@ async def edit_current_user(current_user: User):  # TODO: Finish endpoint
 
 @router.delete('/me')
 async def delete_current_user(current_user: User):  # TODO: Create dependency
+    """ """
+    pass
+
+
+@router.get('/{username}', response_model=User,
+            responses={HTTP_404_NOT_FOUND: {'description': 'User not found'}})
+async def get_user(username: str):
+    """ """
+    pass
+
+
+@router.get('/{username}/resubs', response_model=List[Resub])
+async def get_resubs_owned_by_user(username: str):
+    """ """
+    pass
+
+
+@router.get('/{username}/posts', response_model=List[Post])
+async def get_posts_by_user(username: str):
+    """ """
+    pass
+
+
+@router.get('/{username}/comments', response_model=List[Comment])
+async def get_comments_by_user(username: str):
     """ """
     pass
