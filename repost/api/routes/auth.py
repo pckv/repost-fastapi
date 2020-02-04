40882ac76d3de29cdf6d21fd2626bc9957be07de
Per Christian Kvalvik <pc@kvalvik.no>
2020-02-04 11:34:57 +0100

    Add Token response model for OAuth2

--- repost/api/routes/auth.py
+++ repost/api/routes/auth.py
@@ -2,9 +2,11 @@
 from fastapi import APIRouter, Depends
 from fastapi.security import OAuth2PasswordRequestForm
 
+from repost.api.schemas import OAuth2Token
+
 router = APIRouter()
 
 
-@router.post('/token')
+@router.post('/token', response_model=OAuth2Token)
 async def login(form_data: OAuth2PasswordRequestForm = Depends()):
     pass
--- repost/api/schemas/__init__.py
+++ repost/api/schemas/__init__.py
@@ -2,3 +2,4 @@ from .user import *
 from .resub import *
 from .post import *
 from .comment import *
+from .auth import *
