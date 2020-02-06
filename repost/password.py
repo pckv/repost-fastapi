393c4f27775a24689f52aea38cdb2c5f45d88b1e
Per Christian Kvalvik <pc@kvalvik.no>
2020-02-06 11:21:44 +0100

    Add return types

--- repost/password.py
+++ repost/password.py
@@ -6,11 +6,11 @@ from passlib.context import CryptContext
 password_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
 
-def hash_password(password: str):
+def hash_password(password: str) -> str:
     """Hash the given password using the context scheme."""
     return password_context.hash(password)
 
 
-def verify_password(password: str, hashed_password: str):
+def verify_password(password: str, hashed_password: str) -> bool:
     """Compare the given password with the given hashed password."""
     return password_context.verify(password, hashed_password)
