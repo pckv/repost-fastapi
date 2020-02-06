80ae87babba9e8d4efca0c88e63f0135ce93ddd9
Per Christian Kvalvik <pc@kvalvik.no>
2020-02-06 11:10:24 +0100

    Implement bcrypt hashing functions using passlib.

---
+++ repost/password.py
@@ -0,0 +1,16 @@
+"""Password hashing functions."""
+
+from passlib.context import CryptContext
+
+
+password_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+
+
+def hash_password(password: str):
+    """Hash the given password using the context scheme."""
+    return password_context.hash(password)
+
+
+def verify_password(password: str, hashed_password: str):
+    """Compare the given password with the given hashed password."""
+    return password_context.verify(password, hashed_password)
