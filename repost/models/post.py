56b0ffe757c3cea8b1d2cac8b0fe565b32b822a0
Espen <espenk95@gmail.com>
2020-02-10 16:13:31 +0100

    Post model uses sqlalchemy func for created and edited. Orm mode for post schema

--- repost/models/post.py
+++ repost/models/post.py
@@ -1,4 +1,6 @@
-from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
+import datetime
+
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime, func
 from sqlalchemy.orm import relationship
 
 from . import Base
@@ -11,8 +13,8 @@ class Post(Base):
     title = Column(String)
     url = Column(String, nullable=True)
     content = Column(String, nullable=True)
-    created = Column(DateTime)
-    edited = Column(DateTime, nullable=True)
+    created = Column(DateTime(timezone=True), server_default=func.now())
+    edited = Column(DateTime(timezone=True), onupdate=func.now())
 
     author_id = Column(Integer, ForeignKey('users.id'))
     parent_resub_id = Column(Integer, ForeignKey('resubs.id'))
