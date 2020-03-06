d50e063eb85312241f4757ba858a7104fe68b74a
Per Christian <pc@kvalvik.no>
2020-03-06 15:20:13 +0100

    Rewrite FieldBinder to not overwrite the database model's attributes and allow functions.
    Previously, attributes had to be of the same type in the database model  as in the pydantic model. This meant that conversion from list of votes to an int was not possible.

--- repost/api/schemas/generic.py
+++ repost/api/schemas/generic.py
@@ -1,18 +1,10 @@
 import operator
-from enum import Enum
-from typing import Type
+from typing import Type, Union, Callable, Any
 
 from pydantic.utils import GetterDict
 
 
-class Vote(str, Enum):
-    """Schema for voting paths."""
-    upvote = 'upvote'
-    downvote = 'downvote'
-    novote = 'novote'
-
-
-def bind_orm_fields(**fields: str) -> Type[GetterDict]:
+def bind_orm_fields(**fields: Union[str, Callable]) -> Type[GetterDict]:
     """GetterDict that binds ORM attributes to a Pydantic model.
 
     A field is a key-value mapping where the key is the name of the
@@ -21,21 +13,47 @@ def bind_orm_fields(**fields: str) -> Type[GetterDict]:
 
     Example: bind_orm_fields(owner_username='owner.username')
 
-    This would bind the Pydantic model's owner_username to the
+    This would bind the Pydantic model's owner_username field to the
     database model's owner.username.
+
+    The value can also be a function with a parameter for the ORM
+    model. This function will be called and the returned value
+    will be applied to the Pydantic model.
+
+    Example: bind_orm_fields(votes=lambda obj: sum(v.vote for v in obj.votes))
+
+    This would bind the Pydantic model's votes field to the sum of all
+    elements in the database model's votes list.
     """
 
     class FieldBinder(GetterDict):
-        """GetterDict for setting the owner_username attribute.
+        """GetterDict for resolving attributes from the ORM model.
 
-        Apply this to schemas that need to resolve owner_username from an
-        ORM owner relationship.
+        Apply this to schemas that need to resolve inner attributes
+        from ORM objects, or needs to run a method to resolve any
+        given attribute.
         """
 
         def __init__(self, obj):
+            self.custom_fields = {}
             for field, value in fields.items():
-                setattr(obj, field, operator.attrgetter(value)(obj))
+                if type(value) is str:
+                    self.custom_fields[field] = operator.attrgetter(value)(obj)
+                else:
+                    self.custom_fields[field] = value(obj)
 
-            super(FieldBinder, self).__init__(obj)
+            super().__init__(obj)
+
+        def get(self, key: Any, default: Any = None) -> Any:
+            if key in self.custom_fields:
+                return self.custom_fields[key]
+            else:
+                return super().get(key, default)
+
+        def __getitem__(self, key: str) -> Any:
+            if key in self.custom_fields:
+                return self.custom_fields[key]
+            else:
+                return super().__getitem__(key)
 
     return FieldBinder
