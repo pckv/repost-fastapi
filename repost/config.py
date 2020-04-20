afa823121d7e3eca95a9dd58e0a7d97f7a876ba2
Per Christian Kvalvik <pc@kvalvik.no>
2020-04-20 21:01:54 +0200

    Add REPOST_ as a prefix for environment variables
    This is in order to avoid conflicting environment variable names

--- repost/config.py
+++ repost/config.py
@@ -6,6 +6,7 @@ from pathlib import Path
 from dotenv import load_dotenv
 
 env_path = Path.cwd() / 'config.env'
+env_prefix = 'REPOST_'
 
 
 @dataclass
@@ -21,13 +22,13 @@ class Config:
         # Initialize config file with defaults if it does not exist
         if not env_path.exists():
             with env_path.open(mode='w') as f:
-                f.write('\n'.join(f'{key.upper()}={value}' for key, value in asdict(self).items()) + '\n')
+                f.write('\n'.join(f'{env_prefix}{key.upper()}={value}' for key, value in asdict(self).items()) + '\n')
 
         load_dotenv(dotenv_path=env_path, verbose=True)
 
         # Add environment variables to configurations
         for key, default in asdict(self).items():
-            setattr(self, key, os.getenv(key.upper(), default))
+            setattr(self, key, os.getenv(env_prefix + key.upper(), default))
 
 
 config = Config()
